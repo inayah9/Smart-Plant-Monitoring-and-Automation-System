fc5b295755664a91ac33164333d32e3ef35d5676
Inayah Yasmin <118933064+inayah9@users.noreply.github.com>
2026-03-11 16:46:23 +0000

    added cirtual enviroment,, fake sensor daata and istalled bflask

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 # main flask app
+
+
+from flask import Flask, render_template, jsonify
+from fake_sensors import get_sensor_data
+
+app = Flask(__name__)
+
+@app.route("/")
+def dashboard():
+    sensor_data = get_sensor_data()
+    return render_template("index.html", sensor_data=sensor_data)
+
+@app.route("/api/readings")
+def api_readings():
+    return jsonify(get_sensor_data())
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- test_sensors.py
+++ test_sensors.py
@@ -1,2 +1,14 @@
 # fake senosrs to test the software 
 # Fake humidity, tempreatire and soilm moisture 
+
+
+# Creating the fake data to test the system
+import random
+
+def get_sensor_data():
+    return {
+        "temperature": round(random.uniform(18, 30), 1),
+        "humidity": round(random.uniform(40, 80), 1),
+        "soil_moisture": round(random.uniform(20, 70), 1),
+        "water_level": round(random.uniform(30, 100), 1)
+    }
