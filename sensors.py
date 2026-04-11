c2a10813de918e3664d6d9f093afbaa67f52b046
Inayah <inayahyasmin7@gmail.com>
2026-04-11 02:18:44 +0100

    huh

--- sensors.py
+++ sensors.py
@@ -40,7 +40,7 @@ def _init_ads():
     try:
         import board
         import busio
-        import adafruit_ads1x15.ads1115 as ADS1115
+        import adafruit_ads1x15.ads1115 as ADS
         from adafruit_ads1x15.analog_in import AnalogIn
 
         i2c = busio.I2C(board.SCL, board.SDA)
